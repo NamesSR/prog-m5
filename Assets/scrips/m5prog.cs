7801a9879ce52aad1b20a53923cb6d7312709ac3
Names <40932@ma-web.nl>
2026-09-10 12:13:04 +0200

    first commit

---
+++ Assets/scrips/m5prog.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class m5prog : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
